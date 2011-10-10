f6f2c4c7b3183e5f0867adb02b8fa8c5ee405272
Geoffrey Hichborn <geoff@socialcast.com>
2011-10-10 14:45:32 -0700

    Added block method for running all backgrounded jobs immediately

--- lib/backgrounded.rb
+++ lib/backgrounded.rb
@@ -9,6 +9,14 @@ module Backgrounded
   class << self
     attr_accessor :logger, :handler
   end
+  
+  def self.run_immediately
+    old_handler = Backgrounded.handler
+    Backgrounded.handler = Backgrounded::Handler::InprocessHandler.new
+    yield
+    Backgrounded.handler = old_handler
+  end
+  
 end
 
 # default handler to the basic in process handler
